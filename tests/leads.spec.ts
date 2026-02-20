35d0b4c353bbada0b84bffeba7a66dfc9e4c1c5c
Lucas Dias <lvieira216@gmail.com>
2026-02-19 22:09:24 -0300

    inclusão do page object

--- tests/leads.spec.ts
+++ tests/leads.spec.ts
@@ -1,34 +1,18 @@
 import { test, expect } from "@playwright/test";
 
+import {LandingPage} from "./pages/LandingPage"
 test("deve cadastrar lead de usuário", async ({ page }) => {
-  await page.goto("http://localhost:3000");
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Zombie+/);
-
-  //await page.click('//button[contains(text(), "Aperte o play... se tiver coragem")]');
-  //await page.getByRole('button', {name: 'Aperte o play... se tiver coragem'}).click();
-
-  await page.getByRole('button', { name: /Aperte o play/ }).click();
-
-  await expect(
-    page.getByTestId('modal').getByRole('heading')
-  ).toHaveText('Fila de espera');
-
-  await page.locator('input[name=name]').fill('Lucas Dias');
-  await page.getByPlaceholder('Informe seu email').fill('teste@lucas.com');
-
-  await page.getByRole('button', {name: 'Quero entrar na Fila!'}).click();
-  await page.getByText('Agradecemos').click();
-  await expect(page.getByText(/Agradecemos/)).toBeVisible();
-  /*Forma de pegar o html da página
-  const content = await page.content();
-  console.log(content);*/
-  await expect(page.locator('.toast')).toHaveText
-  ('Agradecemos por compartilhar seus dados conosco. Em breve, nossa equipe entrará em contato!');
-  //tobeHidden - confere de o elemento ficou invisivel 
-  await expect(page.locator('.toast')).toBeHidden({timeout: 5000});
-
+  const landingPage = new LandingPage(page);
+  //Visit
+  await landingPage.visit();
+  //openleandModal
+  await landingPage.opennLeandModal();
+   //submitForm
+  await landingPage.submitForm('Lucas Dias 2', 'teste@qa.com');
+  //toastHaveTest
+  const textToast = "Agradecemos por compartilhar seus dados conosco. Em breve, nossa equipe entrará em contato!"
+  await landingPage.toastHaveTest(textToast);
+  
 });
 test("não deve cadastrar com e-mail inválido", async ({ page }) => {
   await page.goto("http://localhost:3000");
---
+++ tests/pages/LandingPage.ts
@@ -0,0 +1,37 @@
+import { Page,expect } from "@playwright/test";
+
+export class LandingPage {
+  readonly page: Page;
+
+  constructor (page: Page){
+    this.page = page;
+  }
+
+  async visit() {
+    await this.page.goto("http://localhost:3000");
+    await expect(this.page).toHaveTitle(/Zombie+/);
+  }
+  async opennLeandModal() {
+    await this.page.getByRole("button", { name: /Aperte o play/ }).click();
+    //await page.click('//button[contains(text(), "Aperte o play... se tiver coragem")]');
+    //await page.getByRole('button', {name: 'Aperte o play... se tiver coragem'}).click();
+    await expect(this.page.getByTestId("modal").getByRole("heading")).toHaveText(
+      "Fila de espera",
+    );
+  }
+  async submitForm(name: string , email: string) {
+    await this.page.locator("input[name=name]").fill(name);
+    await this.page.getByPlaceholder("Informe seu email").fill(email);
+
+    await this.page.getByRole("button", { name: "Quero entrar na Fila!" }).click();
+  }
+  async toastHaveTest(textToast: string) {
+    await this.page.getByText("Agradecemos").click();
+     /*Forma de pegar o html da página
+      const content = await page.content();
+      console.log(content);*/
+    await expect(this.page.locator(".toast")).toHaveText(textToast);
+    //tobeHidden - confere de o elemento ficou invisivel 
+    await expect(this.page.locator(".toast")).toBeHidden({ timeout: 5000 });
+  }
+}
