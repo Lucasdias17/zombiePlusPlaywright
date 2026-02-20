ca209ad6a5830dbb3cf82822ccf14a6d52f4dae7
Lucas Dias <lvieira216@gmail.com>
2026-02-19 22:41:45 -0300

    inclusão de gancho de poo

--- tests/leads.spec.ts
+++ tests/leads.spec.ts
@@ -1,8 +1,11 @@
-import { test, expect } from "@playwright/test";
+import { test, Page, expect } from "@playwright/test";
+import { LandingPage } from "./pages/LandingPage";
 
-import {LandingPage} from "./pages/LandingPage"
+let landingPage: LandingPage
+test.beforeEach(async({page})=>{
+  landingPage = new LandingPage(page);
+})
 test("deve cadastrar lead de usuário", async ({ page }) => {
-  const landingPage = new LandingPage(page);
   //Visit
   await landingPage.visit();
   //openleandModal
@@ -12,25 +15,18 @@ test("deve cadastrar lead de usuário", async ({ page }) => {
   //toastHaveTest
   const textToast = "Agradecemos por compartilhar seus dados conosco. Em breve, nossa equipe entrará em contato!"
   await landingPage.toastHaveTest(textToast);
-  
+
 });
 test("não deve cadastrar com e-mail inválido", async ({ page }) => {
-  await page.goto("http://localhost:3000");
-
-  await expect(page).toHaveTitle(/Zombie+/);
-
-  await page.getByRole('button', { name: /Aperte o play/ }).click();
-
-  await expect(
-    page.getByTestId('modal').getByRole('heading')
-  ).toHaveText('Fila de espera');
-
-  await page.locator('input[name=name]').fill('Lucas Dias');
-  await page.getByPlaceholder('Informe seu email').fill('testelucas.com');
-
-  await page.getByRole('button', {name: 'Quero entrar na Fila!'}).click();
+  const landingPage = new LandingPage(page);
+  //Visit
+  await landingPage.visit();
+  //openleandModal
+  await landingPage.opennLeandModal();
+   //submitForm
+  await landingPage.submitForm('Lucas Dias 2', 'testeqa.com');
   
-  await expect(page.locator('.alert')).toHaveText('Email incorreto');
+  await landingPage.alertHaveTest('Email incorreto');
 
 });
 test("não deve cadastrar com campos vazio", async ({ page }) => {
@@ -49,5 +45,5 @@ test("não deve cadastrar com campos vazio", async ({ page }) => {
   
   await expect(page.locator('.alert')).toHaveText(['Campo obrigatório', 'Campo obrigatório']);
  
-  page.waitForTimeout(2000);
+  //page.waitForTimeout(2000);
 });
--- tests/pages/LandingPage.ts
+++ tests/pages/LandingPage.ts
@@ -6,32 +6,46 @@ export class LandingPage {
   constructor (page: Page){
     this.page = page;
   }
-
   async visit() {
     await this.page.goto("http://localhost:3000");
     await expect(this.page).toHaveTitle(/Zombie+/);
   }
+
+  
   async opennLeandModal() {
     await this.page.getByRole("button", { name: /Aperte o play/ }).click();
     //await page.click('//button[contains(text(), "Aperte o play... se tiver coragem")]');
     //await page.getByRole('button', {name: 'Aperte o play... se tiver coragem'}).click();
-    await expect(this.page.getByTestId("modal").getByRole("heading")).toHaveText(
-      "Fila de espera",
-    );
+    await expect(this.page.getByTestId("modal").getByRole("heading")).
+    toHaveText("Fila de espera");
   }
+
+
   async submitForm(name: string , email: string) {
     await this.page.locator("input[name=name]").fill(name);
     await this.page.getByPlaceholder("Informe seu email").fill(email);
-
-    await this.page.getByRole("button", { name: "Quero entrar na Fila!" }).click();
+    await this.page.getByRole("button", { name: "Quero entrar na Fila!"}).click();
   }
+
+
   async toastHaveTest(textToast: string) {
+    const toast = this.page.locator(".toast")
+
     await this.page.getByText("Agradecemos").click();
      /*Forma de pegar o html da página
       const content = await page.content();
       console.log(content);*/
-    await expect(this.page.locator(".toast")).toHaveText(textToast);
+    await expect(toast).toHaveText(textToast);
+
     //tobeHidden - confere de o elemento ficou invisivel 
-    await expect(this.page.locator(".toast")).toBeHidden({ timeout: 5000 });
+    //await expect(toast).toBeHidden({ timeout: 5000 });
+
+    //Confere tbm se o elemento ficou invisivel 
+    await expect(toast).not.toBeVisible({ timeout: 5000 });
+  }
+
+
+  async alertHaveTest(target: string){
+    await expect(this.page.locator('.alert')).toHaveText(target);
   }
 }
